80c9022bf2762c54d59a6f04a7b5507920bede22
Sven Vinkemeier <svenvinkemeier@live.de>
2018-04-19 23:52:50 +0200

    Image improvements

---
+++ samples/WpfApp/ImageTests.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for GridTests.xaml
+    /// </summary>
+    public partial class ImageTests : Window
+    {
+        public GridTests()
+        {
+            InitializeComponent();
+        }
+    }
+}
